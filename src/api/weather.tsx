13a8c073537b4be5e42ae018f8d5b6e8ba338830
Thomas Antony S <srtomsam@gmail.com>
2025-05-29 13:17:51 +0530

    updated api and api functions

--- src/api/weather.tsx
+++ src/api/weather.tsx
@@ -1,14 +1,12 @@
-import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const getWeatherByCity = async (city: string) => {
   try {
-    const res = await axios.get(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
-    console.log(res.data);
-    
-    return res.data;
+    const res = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+    const response = await res.json()
+    return response;
   } catch (error) {
     console.error(`Error fetching current weather for ${city}:`, error);
     throw error;
@@ -17,8 +15,9 @@ export const getWeatherByCity = async (city: string) => {
 
 export const getForecastByCity = async (city: string) => {
   try {
-    const res = await axios.get(`${BASE_URL}/forecast?q=${city}&units=metric&appid=${API_KEY}`);
-    return res.data;
+    const res = await fetch(`${BASE_URL}/forecast?q=${city}&units=metric&appid=${API_KEY}`);
+    const response = await res.json()
+    return response;
   } catch (error) {
     console.error(`Error fetching forecast for ${city}:`, error);
     throw error;
