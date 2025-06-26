9dd9175f08b7b53877f64746ddfbab36213f71ff
Thomas Antony S <srtomsam@gmail.com>
2025-06-26 08:16:36 +0530

    spelling mistake fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <p className='w-max mx-auto sm:text-[1.5rem] font-bold my-6'>
-      Developed by - Thomas Anyony S
+      Developed by - Thomas Antony S
     </p>
   )
 }
